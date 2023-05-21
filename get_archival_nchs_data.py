cf7871a289ec83c53994c2eb83e83630ad56b133
Kara Schechtman <kws2121@columbia.edu>
2023-05-20 20:39:50 -0700

    Add handling of the badly formatted dates on 8/21 and 8/27

--- get_archival_nchs_data.py
+++ get_archival_nchs_data.py
@@ -1,7 +1,12 @@
+from io import BytesIO, StringIO
 import boto3
 import numpy as np
 import os
+import pandas as pd
 import requests
+
+# These files have some buggy date formatting
+BAD_FORMATTING = ['20200821232702','20200827232701']
 
 # Request archive.org NVSS metadata 
 nvss = "https://data.cdc.gov/api/views/r8kw-7aab/rows.csv?accessType=DOWNLOAD"
@@ -26,5 +31,14 @@ session = boto3.Session(
 )
 s3 = session.client('s3')
 for date in archived_urls.keys():
-  csv_text = requests.get(archived_urls[date],allow_redirects=True).content
-  s3.put_object(Body=csv_text,Bucket='nvss-deaths',Key='%s.csv' % date)
+  if date in BAD_FORMATTING: # handle the bad date formatting
+    df = pd.read_csv(BytesIO(requests.get(archived_urls[date],allow_redirects=True).content))
+    df['Start week'] = df['Start week'].str.replace('00','20')
+    df['End Week'] = df['End Week'].str.replace('00','20')
+    csv_buffer = StringIO()
+    csv_text=df.to_csv(csv_buffer)
+    s3.put_object(Body=csv_buffer.getvalue(),Bucket='nvss-deaths',Key='%s.csv' % date)
+
+  else:
+    csv_text = requests.get(archived_urls[date],allow_redirects=True).content
+    s3.put_object(Body=csv_text,Bucket='nvss-deaths',Key='%s.csv' % date)
